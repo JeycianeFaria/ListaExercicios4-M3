a21b2fa8668df2333abe4de0d77dc9823f4d7e95
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-27 10:32:57 -0300

    Modelagem da classe coordenadores

--- src/br/com/zup/Coordenadores.java
+++ src/br/com/zup/Coordenadores.java
@@ -1,4 +1,28 @@
 package br.com.zup;
 
-public class Coordenadores {
+public class Coordenadores extends Funcionários {
+
+    //atributos
+    private int qtdprofSupervisionados;
+
+
+    //construtor
+    public Coordenadores() {
+    }
+
+    public Coordenadores(String nome, String cpf, String numRegistro, String orgaoLotacao, double salario) {
+        super(nome, cpf, numRegistro, orgaoLotacao, salario);
+    }
+
+
+    //getters e setters
+    public int getQtdprofSupervisionados() {
+        return qtdprofSupervisionados;
+    }
+
+    public void setQtdprofSupervisionados(int qtdprofSupervisionados) {
+        this.qtdprofSupervisionados = qtdprofSupervisionados;
+    }
+
+
 }
