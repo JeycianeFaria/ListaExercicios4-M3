6483caedb2d9413e8b6319adcc6b70474755e1b6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-27 10:34:31 -0300

    Modelagem da classe Administradores

--- src/br/com/zup/Administradores.java
+++ src/br/com/zup/Administradores.java
@@ -1,4 +1,37 @@
 package br.com.zup;
 
-public class Administradores {
+public class Administradores extends Funcionários{
+
+    //atributos
+    private String funcaoAdministrativa;
+    private String senioridade;
+
+
+    //construtor
+    public Administradores() {
+    }
+
+    public Administradores(String nome, String cpf, String numRegistro, String orgaoLotacao, double salario, String funcaoAdministrativa, String senioridade) {
+        super(nome, cpf, numRegistro, orgaoLotacao, salario);
+        this.funcaoAdministrativa = funcaoAdministrativa;
+        this.senioridade = senioridade;
+    }
+
+
+    //getters e setters
+    public String getFuncaoAdministrativa() {
+        return funcaoAdministrativa;
+    }
+
+    public void setFuncaoAdministrativa(String funcaoAdministrativa) {
+        this.funcaoAdministrativa = funcaoAdministrativa;
+    }
+
+    public String getSenioridade() {
+        return senioridade;
+    }
+
+    public void setSenioridade(String senioridade) {
+        this.senioridade = senioridade;
+    }
 }
