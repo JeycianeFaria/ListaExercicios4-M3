4e5517fe29c764484444e90b70475d14786defab
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-27 10:26:52 -0300

    Modelagem da classe funcionário

--- src/br/com/zup/Funcionários.java
+++ src/br/com/zup/Funcionários.java
@@ -2,5 +2,66 @@ package br.com.zup;
 
 public class Funcionários {
 
+    //atributos
+    private String nome;
+    private String cpf;
+    private String numRegistro;
+    private String orgaoLotacao;
+    private double salario;
+
+
+    //construtor
+    public Funcionários() {
+    }
+
+    public Funcionários(String nome, String cpf, String numRegistro, String orgaoLotacao, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numRegistro = numRegistro;
+        this.orgaoLotacao = orgaoLotacao;
+        this.salario = salario;
+    }
+
+
+    //getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumRegistro() {
+        return numRegistro;
+    }
+
+    public void setNumRegistro(String numRegistro) {
+        this.numRegistro = numRegistro;
+    }
+
+    public String getOrgaoLotacao() {
+        return orgaoLotacao;
+    }
+
+    public void setOrgaoLotacao(String orgaoLotacao) {
+        this.orgaoLotacao = orgaoLotacao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 
 }
